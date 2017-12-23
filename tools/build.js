2367c512f8109896ee734a6a90d5026be4029cdc
ktnyt <kotone@sfc.keio.ac.jp>
2017-12-23 12:11:27 +0900

    Prepare for release

--- tools/build.js
+++ tools/build.js
@@ -21,22 +21,22 @@ exec('babel modules -d es --ignore __tests__', {
   BABEL_ENV: 'es'
 })
 
-console.log('\nBuilding react-rest.js ...')
+console.log('\nBuilding react-rest-client.js ...')
 
-exec('rollup -c -f umd -o umd/react-rest.js', {
+exec('rollup -c -f umd -o umd/react-rest-client.js', {
   BABEL_ENV: 'umd',
   NODE_ENV: 'development'
 })
 
-console.log('\nBuilding react-rest.min.js ...')
+console.log('\nBuilding react-rest-client.min.js ...')
 
-exec('rollup -c -f umd -o umd/react-rest.min.js', {
+exec('rollup -c -f umd -o umd/react-rest-client.min.js', {
   BABEL_ENV: 'umd',
   NODE_ENV: 'production'
 })
 
 const size = gzipSize.sync(
-  fs.readFileSync('umd/react-rest.min.js')
+  fs.readFileSync('umd/react-rest-client.min.js')
 )
 
 console.log('\ngzipped, the UMD build is %s', prettyBytes(size))
